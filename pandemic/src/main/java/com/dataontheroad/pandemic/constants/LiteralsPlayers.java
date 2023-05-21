278fe2775074c46b2eb8352d09858806dd36f0bb
jimenezict <jimenezict@gmail.com>
2023-05-21 18:56:06 +0200

    Adding literals and implementation description on the readme

--- pandemic/src/main/java/com/dataontheroad/pandemic/constants/LiteralsPlayers.java
+++ pandemic/src/main/java/com/dataontheroad/pandemic/constants/LiteralsPlayers.java
@@ -8,10 +8,21 @@ public class LiteralsPlayers {
     public static final String QUARANTINE_COLOR = "Dark Green";
     public static final String QUARANTINE_NAME = "Quarantine Specialist";
     public static final String QUARANTINE_DESCRIPTION = "Prevent all disease cube placements (and outbreaks) in the city you are in and adjacent cities.";
-    public static final String CONTINGENCY_COLOR = "";
-    public static final String CONTINGENCY_NAME = "";
-    public static final String CONTINGENCY_DESCRIPTION = "";
+    public static final String CONTINGENCY_COLOR = "Blue";
+    public static final String CONTINGENCY_NAME = "Contingent Planner";
+    public static final String CONTINGENCY_DESCRIPTION = "As an action, take any discarded event card and store it on this card, when you play the stored Event card, remove from the game";
     public static final String MEDIC_COLOR = "Orange";
     public static final String MEDIC_NAME = "Medic";
     public static final String MEDIC_DESCRIPTION = "Remove all cubes of a single color when you treat a city. Administer known cures for free";
+    public static final String DISPATCHER_COLOR = "Pink";
+    public static final String DISPATCHER_NAME = "Dispatcher";
+    public static final String DISPATCHER_DESCRIPTION = "Move another player's pawn as if it were yours. As an action, move any pwn to a city with another pawn";
+    public static final String OPERATIONS_COLOR = "Soft Green";
+    public static final String OPERATIONS_NAME = "Operations Expert";
+    public static final String OPERATIONS_DESCRIPTION = "As an action, build a research station in the city you are in (no City card needed), once per turn as an action, move from a reserch station to any city by discarding any City card";
+    public static final String RESEARCHER_COLOR = "Brown";
+    public static final String RESEARCHER_NAME = "Researcher";
+    public static final String RESEARCHER_DESCRIPTION = "As an action, you may give (or a player can take) any city card from your han. You must both be in the same city. The card does not have to match the city you are in";
+
+
 }
