4c6117f6eafbb44b6b6886721e213c09d16ec265
jimenezict <jimenezict@gmail.com>
2023-06-20 08:49:41 +0200

    Logic for creating of games Needs to improve in: * Tests (to decide mockmvc) * Add exception when numPandemic is invalid (in the service layer) * Use the text of the exception as the one to be returned

---
+++ pandemic/src/main/java/com/dataontheroad/pandemic/game/api/rest/GameEndPoint.java
@@ -0,0 +1,30 @@
+package com.dataontheroad.pandemic.game.api.rest;
+
+import com.dataontheroad.pandemic.game.service.GameServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class GameEndPoint {
+
+    @Autowired
+    GameServiceImpl gameService;
+
+    @GetMapping("/game/create/players/{numPlayers}/pandemic/{numPandemics}")
+    ResponseEntity<?> createGame(@PathVariable int numPlayers, @PathVariable int numPandemics) {
+        UUID uuid;
+
+        try {
+            uuid = gameService.createGame(numPandemics, numPlayers);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error creating game, players should be between 2 and 4");
+        }
+        return ResponseEntity.ok().body("created game with id " + uuid);
+    }
+
+}
--- pandemic/src/main/java/com/dataontheroad/pandemic/game/service/GameServiceImpl.java
+++ pandemic/src/main/java/com/dataontheroad/pandemic/game/service/GameServiceImpl.java
@@ -4,6 +4,7 @@ import com.dataontheroad.pandemic.game.api.model.GameResponseDTO;
 import com.dataontheroad.pandemic.game.persistence.GamePersistenceOnHashMap;
 import com.dataontheroad.pandemic.game.persistence.model.GameDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -11,6 +12,7 @@ import java.util.UUID;
 import static com.dataontheroad.pandemic.game.service.ConvertGamesDTO.convertGameDTO;
 import static com.dataontheroad.pandemic.model.board.BoardCreationHelper.addEpidemicCards;
 
+@Service
 public class GameServiceImpl implements IGameService {
 
     @Autowired
