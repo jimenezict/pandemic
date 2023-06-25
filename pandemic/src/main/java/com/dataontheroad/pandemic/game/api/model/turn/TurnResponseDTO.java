b6a622bfe617ffcdb569030273d0bd7247ac5f26
jimenezict <jimenezict@gmail.com>
2023-06-25 23:12:32 +0200

    TurnServiceImpl

--- pandemic/src/main/java/com/dataontheroad/pandemic/game/api/model/turn/TurnResponseDTO.java
+++ pandemic/src/main/java/com/dataontheroad/pandemic/game/api/model/turn/TurnResponseDTO.java
@@ -1,14 +1,22 @@
 package com.dataontheroad.pandemic.game.api.model.turn;
 
+import com.dataontheroad.pandemic.actions.action_factory.Action;
 import com.dataontheroad.pandemic.game.persistence.model.TurnInformation;
 import com.dataontheroad.pandemic.model.player.Player;
+
+import java.util.List;
 
 public class TurnResponseDTO {
 
     private int missingTurns;
     private Player activePlayer;
 
-    public void setTurnInformation(TurnInformation turnInformation) {
+    private List<Action> actionList;
+
+    public void setTurnInformation(TurnInformation turnInformation, List<Action> actionList
+
+
+     ) {
         this.missingTurns = turnInformation.getMissingTurns();
         this.activePlayer = turnInformation.getActivePlayer();
     }
--- pandemic/src/main/java/com/dataontheroad/pandemic/game/service/TurnServiceImpl.java
+++ pandemic/src/main/java/com/dataontheroad/pandemic/game/service/TurnServiceImpl.java
@@ -1,13 +1,17 @@
 package com.dataontheroad.pandemic.game.service;
 
+import com.dataontheroad.pandemic.actions.action_factory.Action;
 import com.dataontheroad.pandemic.game.api.model.turn.TurnResponseDTO;
 import com.dataontheroad.pandemic.game.persistence.GamePersistenceOnHashMap;
 import com.dataontheroad.pandemic.game.persistence.model.GameDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import static com.dataontheroad.pandemic.game.ActionService.getListOfActions;
 import static java.util.Objects.isNull;
 
 @Service
@@ -18,6 +22,16 @@ public class TurnServiceImpl implements ITurnService {
 
     @Override
     public TurnResponseDTO getTurnServiceInformation(UUID gameId) {
-        return null;
+        GameDTO gameDTO = gamePersistence.getGameById(gameId);
+        TurnResponseDTO turnResponseDTO = new TurnResponseDTO();
+
+        if(!isNull(gameDTO)) {
+            List<Action> actionList = getListOfActions(gameDTO.getTurnInformation().getActivePlayer(),
+                    gameDTO.getBoard().getVirusList(),
+                    gameDTO.getBoard().getBoardCities().stream().filter(city -> city.getHasCenter()).collect(Collectors.toList()),
+                    gameDTO.getBoard().getPlayers().stream().filter(player -> player.getCity().equals(gameDTO.getTurnInformation().getActivePlayer())).collect(Collectors.toList()));
+            turnResponseDTO.setTurnInformation(gameDTO.getTurnInformation(), actionList);
+        }
+        return turnResponseDTO;
     }
 }
