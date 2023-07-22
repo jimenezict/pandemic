8ae25400c57c17e1580aa1a090ec705392e4cf3e
jimenezict <jimenezict@gmail.com>
2023-07-22 20:38:58 +0200

    Enum for connections between cities

--- pandemic/src/test/java/com/dataontheroad/pandemic/model/city/CityFactoryTest.java
+++ pandemic/src/test/java/com/dataontheroad/pandemic/model/city/CityFactoryTest.java
@@ -17,24 +17,54 @@ class CityFactoryTest {
 
         assertEquals(48, cityList.size());
         assertTrue(atlanta.getHasCenter());
+
+        assertEquals(4, sizeConnectionByCityName("Algiers", cityList));
         assertEquals(3, sizeConnectionByCityName("Atlanta", cityList));
+        assertEquals(5, sizeConnectionByCityName("Baghdad", cityList));
+        assertEquals(2, sizeConnectionByCityName("Beijing", cityList));
+        assertEquals(5, sizeConnectionByCityName("Bogota", cityList));
+        assertEquals(2, sizeConnectionByCityName("Buenos Aires", cityList));
+        assertEquals(5, sizeConnectionByCityName("Cairo", cityList));
+        assertEquals(5, sizeConnectionByCityName("Chennai", cityList));
         assertEquals(5, sizeConnectionByCityName("Chicago", cityList));
-        assertEquals(3, sizeConnectionByCityName("Montreal", cityList));
-        assertEquals(4, sizeConnectionByCityName("Washington", cityList));
-        assertEquals(4, sizeConnectionByCityName("San Francisco", cityList));
+        assertEquals(5, sizeConnectionByCityName("Delhi", cityList));
+        assertEquals(4, sizeConnectionByCityName("Essen", cityList));
+        assertEquals(4, sizeConnectionByCityName("Ho Chi Minh City", cityList));
+        assertEquals(6, sizeConnectionByCityName("Hong Kong", cityList));
+        assertEquals(6, sizeConnectionByCityName("Istanbul", cityList));
+        assertEquals(4, sizeConnectionByCityName("Jakarta", cityList));
+        assertEquals(2, sizeConnectionByCityName("Johannesburg", cityList));
+        assertEquals(5, sizeConnectionByCityName("Karachi", cityList));
+        assertEquals(4, sizeConnectionByCityName("Khartoum", cityList));
+        assertEquals(3, sizeConnectionByCityName("Kinshasa", cityList));
+        assertEquals(4, sizeConnectionByCityName("Kolkata", cityList));
+        assertEquals(3, sizeConnectionByCityName("Lagos", cityList));
+        assertEquals(3, sizeConnectionByCityName("Lima", cityList));
+        assertEquals(4, sizeConnectionByCityName("London", cityList));
         assertEquals(4, sizeConnectionByCityName("Los Angeles", cityList));
+        assertEquals(5, sizeConnectionByCityName("Madrid", cityList));
+        assertEquals(5, sizeConnectionByCityName("Manila", cityList));
         assertEquals(5, sizeConnectionByCityName("Mexico City", cityList));
         assertEquals(4, sizeConnectionByCityName("Miami", cityList));
+        assertEquals(3, sizeConnectionByCityName("Milan", cityList));
+        assertEquals(3, sizeConnectionByCityName("Montreal", cityList));
+        assertEquals(3, sizeConnectionByCityName("Moscow", cityList));
+        assertEquals(3, sizeConnectionByCityName("Mumbai", cityList));
         assertEquals(4, sizeConnectionByCityName("New York", cityList));
-        assertEquals(5, sizeConnectionByCityName("Bogota", cityList));
-        assertEquals(3, sizeConnectionByCityName("Lima", cityList));
+        assertEquals(2, sizeConnectionByCityName("Osaka", cityList));
+        assertEquals(5, sizeConnectionByCityName("Paris", cityList));
+        assertEquals(3, sizeConnectionByCityName("Riyadh", cityList));
+        assertEquals(4, sizeConnectionByCityName("San Francisco", cityList));
         assertEquals(1, sizeConnectionByCityName("Santiago", cityList));
         assertEquals(4, sizeConnectionByCityName("Sao Paulo", cityList));
-        assertEquals(2, sizeConnectionByCityName("Buenos Aires", cityList));
-        assertEquals(3, sizeConnectionByCityName("Lagos", cityList));
-        assertEquals(3, sizeConnectionByCityName("Kinshasa", cityList));
-        assertEquals(2, sizeConnectionByCityName("Johannesburg", cityList));
-        assertEquals(4, sizeConnectionByCityName("Khartoum", cityList));
+        assertEquals(3, sizeConnectionByCityName("Seoul", cityList));
+        assertEquals(5, sizeConnectionByCityName("Shanghai", cityList));
+        assertEquals(3, sizeConnectionByCityName("St. Petersburg", cityList));
+        assertEquals(3, sizeConnectionByCityName("Sydney", cityList));
+        assertEquals(4, sizeConnectionByCityName("Taipei", cityList));
+        assertEquals(4, sizeConnectionByCityName("Tehran", cityList));
+        assertEquals(4, sizeConnectionByCityName("Tokyo", cityList));
+        assertEquals(4, sizeConnectionByCityName("Washington", cityList));
     }
 
     private int sizeConnectionByCityName(String name, List<City> cityList) {
