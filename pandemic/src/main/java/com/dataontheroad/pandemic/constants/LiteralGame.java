9329310525206fbb413f9bd0fa9153c82b71d994
jimenezict <jimenezict@gmail.com>
2023-09-09 12:46:47 +0200

    correcting literals for WRONG_PLAYERS and WRONG_EPIDEMIC_CARDS

--- pandemic/src/main/java/com/dataontheroad/pandemic/constants/LiteralGame.java
+++ pandemic/src/main/java/com/dataontheroad/pandemic/constants/LiteralGame.java
@@ -8,8 +8,8 @@ public class LiteralGame {
     public static final String TURN_ENDPOINT_NAME = "Turn End Point";
     public static final String GAME_ENDPOINT_NAME = "Game End Point";
     public static final String GAME_NOT_FOUND = "Game not found";
-    public static final String WRONG_PLAYERS = "Error creating game, epidemic cards should be between 0 and 6";
-    public static final String WRONG_EPIDEMIC_CARDS = "Error creating game, players should be between 2 and 4";
+    public static final String WRONG_EPIDEMIC_CARDS = "Error creating game, epidemic cards should be between 0 and 6";
+    public static final String WRONG_PLAYERS = "Error creating game, players should be between 2 and 4";
     public static final String SUCCESS_GAME = "Game created";
     public static final String SUCCESS_ACTION = "Action executed correctly";
     public static final String TURN_WRONG_PLAYER = "Error executing action, requester is not the active player";
